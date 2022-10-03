19710a29a25f5148af3ca5fb6c629c803ee6219a
christoph.hafner@hitachienergy.com <christoph.hafner@hitachienergy.com>
2022-10-04 00:06:37 +0200

    References to external packages "" and "" inlined and attributes made internal to avoid conflicts with other imports that define these attributes.

---
+++ Source/Hafner.Tools.SetTracker/GuidAttribute/GuidAttribute.cs
@@ -0,0 +1,39 @@
+﻿/********************************************************************************************************************************
+ * Credits to Microsoft who provided the code this file is based on through their reference source repository, see
+ * "https://referencesource.microsoft.com/#mscorlib/system/runtime/interopservices/attributes.cs,e803ebb9a4b76468".
+ * The original file is MIT licensed.
+ ********************************************************************************************************************************/
+
+// ==++==
+// 
+//   Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+// ==--==
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+namespace System.Runtime.InteropServices {
+
+    /// <summary>
+    /// Supplies an explicit System.Guid when an automatic GUID is undesirable.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Struct | AttributeTargets.Delegate, Inherited = false)]
+    [ComVisible(true)]
+    internal sealed class GuidAttribute : Attribute {
+
+        /// <summary>
+        /// Initializes a new instance of the System.Runtime.InteropServices.GuidAttribute class with the specified GUID.
+        /// </summary>
+        /// <param name="guid">The System.Guid to be assigned.</param>
+        public GuidAttribute(string guid) {
+            Value = guid;
+        }
+
+        /// <summary>
+        /// Gets the System.Guid of the type or assembly.
+        /// </summary>
+        public string Value { get; }
+
+    }
+
+}
